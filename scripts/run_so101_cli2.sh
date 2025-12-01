47448a541ea8a9c2a7a6d7a1b621e37479a7593e
ustbxcj <2248190184@qq.com>
2025-12-01 17:43:59 +0800

    测试

--- scripts/run_so101_cli2.sh
+++ scripts/run_so101_cli2.sh
@@ -1,7 +1,7 @@
-conda activate op
+# conda activate op
 
-python operating_platform/core/main.py \
-    --robot.type=so101_2 \
-    --record.repo_id="so101-tes-2t" \
-    --record.single_task="start and test so101 arm2."
+# python operating_platform/core/main.py \
+#     --robot.type=so101_2 \
+#     --record.repo_id="so101-tes-2t" \
+#     --record.single_task="start and test so101 arm2."
 
